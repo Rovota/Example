1ce092a5f9ee98fa35458ce7ed6e95ce6c86f099
Léandro Tijink <leandro.tijink@gmail.com>
2024-11-25 13:20:43 +0100

    added: header partial

---
+++ resources/templates/partials/header.php
@@ -0,0 +1,8 @@
+<header>
+	<container>
+		<content>
+			<h1><?= __('Hello!') ?></h1>
+			<p><?= __('This is the starting point for your new adventure.') ?></p>
+		</content>
+	</container>
+</header>
